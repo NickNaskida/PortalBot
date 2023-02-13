d6697142387cf63811737d3904913efbb3411940
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2023-02-13 13:11:02 +0400

    refactor: notifications

--- src/fill_db.py
+++ src/fill_db.py
@@ -1,12 +1,10 @@
-import json
 import logging
 import asyncio
 
-from src.redis import add_prices, get_yesterday_prices, get_subscribed_users
+from src.redis import add_prices
 from src.parser import parse_portal_data
 from src.bot import redis
-from src.utils import format_prices, format_price
-from src.notify import send_notification
+from src.notify import compare_and_notify
 
 
 async def fill_db_and_notify() -> None:
@@ -18,30 +16,9 @@ async def fill_db_and_notify() -> None:
     except Exception as e:
         logging.error(f"Error while filling db: {e}")
 
-    yesterday_prices = await get_yesterday_prices(redis)
-    change = False
+    await compare_and_notify(data)
 
-    if yesterday_prices:
-        yesterday_prices = json.loads(yesterday_prices)
-
-        for fuel, price in data.items():
-            yesterday_price = yesterday_prices.get(fuel)
-            if price < yesterday_price:
-                data[fuel] = f"{price} ↓ ({format_price(float(yesterday_price) - float(price))})"
-                change = True
-            elif price > yesterday_price:
-                data[fuel] = f"{price} ↑ ({format_price(float(yesterday_price) - float(price))})"
-
-        if change:
-            yesterday_prices = format_prices(yesterday_prices)
-            data = format_prices(data)
-            message = f"დაფიქსირდა ფასების ცვლილება:\n\nძველი ფასები:\n\n{yesterday_prices}\n\nახალი ფასები:\n\n{data}"
-            subscribers = await get_subscribed_users(redis)
-
-            for user in subscribers:
-                print(f"Sending notification to {user}...")
-                await send_notification(user, message)
-                await asyncio.sleep(.05)
+    # TODO: check if notification was sent already
 
 
 if __name__ == '__main__':
--- src/notify.py
+++ src/notify.py
@@ -1,9 +1,13 @@
+import json
 import logging
 
 import asyncio
 from aiogram import exceptions
 
 from src.config import bot
+from src.redis import get_yesterday_prices, get_subscribed_users
+from src.bot import redis
+from src.utils import format_prices, format_price
 
 
 async def send_notification(user_id, message) -> None:
@@ -26,3 +30,32 @@ async def send_notification(user_id, message) -> None:
         logging.exception(f"Target [ID:{user_id}]: failed")
     else:
         logging.info(f"Target [ID:{user_id}]: success")
+
+
+async def compare_and_notify(data):
+    yesterday_prices = await get_yesterday_prices(redis)
+    change = False
+
+    if yesterday_prices:
+        yesterday_prices = json.loads(yesterday_prices)
+
+        for fuel, price in data.items():
+            yesterday_price = yesterday_prices.get(fuel)
+
+            if price != yesterday_price:
+                change = True
+
+            if price < yesterday_price:
+                data[fuel] = f"{price} ↓ ({format_price(float(yesterday_price) - float(price))})"
+            elif price > yesterday_price:
+                data[fuel] = f"{price} ↑ ({format_price(float(yesterday_price) - float(price))})"
+
+        if change:
+            yesterday_prices = format_prices(yesterday_prices)
+            data = format_prices(data)
+            message = f"დაფიქსირდა ფასების ცვლილება:\n\nძველი ფასები:\n\n{yesterday_prices}\n\nახალი ფასები:\n\n{data}"
+            subscribers = await get_subscribed_users(redis)
+
+            for user in subscribers:
+                await send_notification(user, message)
+                await asyncio.sleep(.05)
